61c93b73d610cb9b87087c6545247164235c0ab6
Michael Koerber <michaelkoerber@Michaels-MacBook-Pro.local>
2018-02-12 14:11:45 -0600

    Finished mini-project

---
+++ src/ducks/counter.js
@@ -0,0 +1,73 @@
+//Constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const UNDO = "UNDO";
+const REDO = "REDO";
+
+//Action Creators
+export function increment(amount) {
+    return {
+        type: INCREMENT,
+        amount
+    };
+}
+
+export function decrement(amount) {
+    return {
+        type: DECREMENT,
+        amount
+    }
+}
+
+export function undo() {
+    return { type: UNDO };
+}
+
+export function redo() {
+    return { type: REDO };
+}
+
+
+//Initial State
+let initialState = {
+    currentValue: 0,
+    futureValues: [],
+    previousValues: []
+};
+
+
+//Reducer
+export default function counter(state = initialState, action) {
+    switch(action.type) {
+        case INCREMENT:
+        return {
+            currentValue: state.currentValue + action.amount, 
+            futureValues: [],
+            previousValues: [ state.currentValue, ...state.previousValues ]
+          };
+
+        case DECREMENT:
+        return {
+            currentValue: state.currentValue - action.amount,
+            futureValues: [],
+            previousValues: [ state.currentValue, ...state.previousValues ]
+          };
+
+        case UNDO:
+          return {
+              currentValue: state.previousValues[ 0 ],
+              futureValues: [state.currentValue, ...state.futureValues],
+              previousValues: state.previousValues.slice( 1, state.previousValues.length )
+          };
+
+        case REDO:
+        return {
+            currentValue: state.futureValues[ 0 ],
+            futureValues: state.futureValues.slice( 1, state.futureValues.length ),
+            previousValues: [ state.currentValue, ...state.previousValues ]
+        };
+         
+        default:
+          return state;  
+    }
+}
